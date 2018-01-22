ee2fb2132e4f9be4e928515fc5669058d7754141
Adrian <colper89@gmail.com>
2018-01-22 10:49:18 +0100

    Empty MVC Project - To - Repository

---
+++ Blog_VT18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog_VT18.Startup))]
+namespace Blog_VT18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
