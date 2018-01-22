645b2900fa9fa76f1780749c1f712b3e2ada2b2e
Adrian <colper89@gmail.com>
2018-01-22 16:46:31 +0100

    No description

--- Blog_VT18/Startup.cs
+++ Blog_VT18/Startup.cs
@@ -2,12 +2,9 @@
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Blog_VT18.Startup))]
-namespace Blog_VT18
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
+namespace Blog_VT18 {
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
         }
     }
