c34569525796f133051a8b5f490d9e589dfac512
Adrian <colper89@gmail.com>
2018-01-25 15:17:03 +0100

    Merge from AddUser

--- Blog_VT18/Migrations/Configuration.cs
+++ Blog_VT18/Migrations/Configuration.cs
@@ -1,15 +1,18 @@
 namespace Blog_VT18.Migrations {
+    using Blog_VT18.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Blog_VT18.Models.ApplicationDbContext> {
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext> {
         public Configuration() {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Blog_VT18.Models.ApplicationDbContext context) {
+        protected override void Seed(ApplicationDbContext context) {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -22,6 +25,26 @@ namespace Blog_VT18.Migrations {
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if(!roleManager.RoleExists("Administrator")) {
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+            }
+
+            var user = new ApplicationUser {
+                Name = "Admin",
+                UserName = "admin@user.com",
+                Email = "admin@user.com"
+            };
+
+            var adminUser = userManager.Create(user, "User1!");
+            //userManager.CreateAsync(user, "User1!").Wait();
+
+            if(adminUser.Succeeded) { var result1 = userManager.AddToRole(user.Id, "Administrator"); }
+            base.Seed(context);
         }
     }
 }
