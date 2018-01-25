7f7dc89b2611a4d506e542321c6da0b3f318c6d0
Henrik Wallgren <Heeenriik@live.se>
2018-01-25 14:24:44 +0100

    Börjat create post
    Skapat basecontroller, db context och en blogpostcontroller , påbörjat create blogpost

---
+++ Blog_VT18/Controllers/BlogPostController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using System.Data.Entity;
+using Blog_VT18.Models;
+
+namespace Blog_VT18.Controllers
+{
+    public class BlogPostController : BaseController
+    {
+        // GET: BlogPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(string content, string Category)
+        {
+            Categories category = db.Categories.Single(x => x.Name == Category);
+            BlogPost newPost = new BlogPost();
+            newPost.Category = category;
+            newPost.Content = content;
+                return View(newPost);
+        }
+    }
+}
