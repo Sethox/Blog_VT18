24d6ed60cea923a807acf492ab48302b59cff50b
Adrian <colper89@gmail.com>
2018-01-26 11:40:59 +0100

    Repository created

---
+++ Blog_VT18/Controllers/RepositoryManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Blog_VT18.Models;
+
+namespace Blog_VT18.Controllers {
+    public class RepositoryManager {
+        ApplicationDbContext db;
+        public ApplicationUser usr { get {
+                var userID = HttpContext.Current.User.Identity.GetUserId();
+                var currentUser = db.Users.Where(x => x.Id == userID).FirstOrDefault();
+                return currentUser;
+            } }
+
+
+        public RepositoryManager() { this.db = new ApplicationDbContext(); }
+
+        public void setCurrentUser(ApplicationUser modifyUsr) {
+            this.usr.Name = modifyUsr.Name;
+            this.usr.UserName = modifyUsr.UserName;
+            this.usr.Email = modifyUsr.Email;
+            this.db.SaveChanges();
+        }
+
+        /// <summary>
+        /// This creates a new catagory.
+        /// </summary>
+        /// <param name="post">This is the model to update the database.</param>
+        public void newCatagory(BlogPost post) {
+            if(post != null) {
+                this.db.BlogPosts.Add(post);
+                this.db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Disposing the classes properties.
+        /// </summary>
+        protected void Dispose(bool disposing) {
+            if(disposing && this.db != null ) {
+                this.db.Dispose();
+                this.db = null;
+            }
+        }
+    }
+}
