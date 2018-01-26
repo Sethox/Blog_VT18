ad20b7a9ff533b1349d33d0a7c77148a01aaabcc
AnnaLundqvist <anna.lundqvist@live.se>
2018-01-26 11:14:46 +0100

    Kalender klar

---
+++ Blog_VT18/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Blog_VT18.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_VT18.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+
+}
