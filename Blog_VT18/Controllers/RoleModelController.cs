0b82f847f7b240298c075bd9dec7db50c52df4e5
junieboden <35684793+junieboden@users.noreply.github.com>
2018-02-12 15:04:22 +0100

    syntaxkorrektioner

--- Blog_VT18/Controllers/RoleModelController.cs
+++ Blog_VT18/Controllers/RoleModelController.cs
@@ -15,31 +15,29 @@ namespace Blog_VT18.Controllers {
     [Authorize(Roles = "Administrator")]
     public class RoleModelController : Controller {
         private RepositoryManager manager;
-
-        public RoleModelController() { this.manager = new RepositoryManager(); }
+        public RoleModelController() { manager = new RepositoryManager(); }
 
         // GET: RoleModel
         public ActionResult Index() {
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(manager.db));
-            var usrLst = manager.usrList();
-            var remodelLst = new List<RoleModel>();
-            foreach (var i in usrLst) {
-                var r = userManager.GetRoles(i.Id).ToList().First();
-                remodelLst.Add(new RoleModel { ID = i.Id, Name = i.UserName, Role = r, ByWhom = manager.usr, IsEnabled = i.IsEnabled });
+            var userList = manager.usrList();
+            var roleModelList = new List<RoleModel>();
+            foreach (var user in userList) {
+                var role = userManager.GetRoles(user.Id).ToList().First();
+                roleModelList.Add(new RoleModel { ID = user.Id, Name = user.UserName, Role = role, ByWhom = manager.usr, IsEnabled = user.IsEnabled });
             }
-            return View(remodelLst.ToList());
+            return View(roleModelList.ToList());
         }
 
         // GET: RoleModel/Edit/5
         public ActionResult Edit(string id) {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var usr = this.manager.db.Users.Find(id);
-            var dl = new List<SelectListItem>();
-            foreach(var i in manager.db.Roles.ToList()) {
-                dl.Add(new SelectListItem { Value = i.Name, Text = i.Name });
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var selectList = new List<SelectListItem>();
+            foreach(var identityRole in manager.db.Roles.ToList()) {
+                selectList.Add(new SelectListItem { Value = identityRole.Name, Text = identityRole.Name });
             }
-            RoleModel roleModel = new RoleModel { ID = manager.usr.Id, Name = manager.usr.UserName, Role = manager.getRole(id), ByWhom = manager.usr, Roles = dl };
-            if (roleModel == null) return HttpNotFound();
+            RoleModel roleModel = new RoleModel { ID = manager.usr.Id, Name = manager.usr.UserName, Role = manager.getRole(id), ByWhom = manager.usr, Roles = selectList };
             return View(roleModel);
         }
 
@@ -56,13 +54,14 @@ namespace Blog_VT18.Controllers {
 
         // GET: RoleModel/Delete/5
         public ActionResult Delete(string id) {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             manager.changeIsEnabled(id);
             return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing) {
-            if (disposing) this.manager.db.Dispose();
+            if (disposing) manager.db.Dispose();
             base.Dispose(disposing);
         }
     }
